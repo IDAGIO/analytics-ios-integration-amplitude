c6719550fae2752e6127117f2e2f8b6628ce9f76
Franco Octavianus <franco.octavianus@gmail.com>
2021-06-21 01:19:04 +1000

    Simplify names

--- Package.swift
+++ Package.swift
@@ -4,16 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Segment-Amplitude",
+    name: "Segment_Amplitude",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_13)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Segment-Amplitude",
-            targets: ["Segment-Amplitude"]),
+            name: "Segment_Amplitude",
+            targets: ["Segment_Amplitude"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Segment-Amplitude",
+            name: "Segment_Amplitude",
             dependencies: ["Amplitude", "Segment"])
     ]
 )
